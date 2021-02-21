54acfe19be557efafb0f651db4350c8b55eab06d
Pau Garcia Martínez <etpau@hotmail.es>
2021-02-21 11:48:26 +0100

    Animate the transition between the elements

--- m10.3-cash-converter/src/App.vue
+++ m10.3-cash-converter/src/App.vue
@@ -1,29 +1,29 @@
 <!-- TEMPLATE -->
 <template>
+  <transition name="switch" mode="out-in">
+    <Modal v-if="showModal" :sendInput="input">
+      <template v-slot:header>
+        <h1>{{ title }}</h1>
+      </template>
+      
+      <template v-slot:body>
+        <p class="subtitle">{{ subtitle }}</p>
+        <input type="number" class="converter" v-model.number="input"> 
+      </template>
 
-  <Modal v-if="showModal" :sendInput="input">
-    <template v-slot:header>
-      <h1>{{ title }}</h1>
+      <template v-slot:footer>
+        <button @click="toggleShowModal" class="modal-btn">Close</button>
+      </template> 
+    </Modal>
+
+    <template v-else>
+      <div class="open-container">
+        <img src="./assets/money.jpg" class="money">
+        <h2>{{ alternativeTitle }}</h2>
+        <button @click="toggleShowModal" class="modal-btn">Open</button>
+      </div>
     </template>
-    
-    <template v-slot:body>
-      <p class="subtitle">{{ subtitle }}</p>
-      <input type="number" class="converter" v-model.number="input"> 
-    </template>
-
-    <template v-slot:footer>
-      <button @click="toggleShowModal" class="modal-btn">Close</button>
-    </template> 
-  </Modal>
-
-  <template v-else>
-    <div class="open-container">
-      <img src="./assets/money.jpg" class="money">
-      <h2>{{ alternativeTitle }}</h2>
-      <button @click="toggleShowModal" class="modal-btn">Open</button>
-    </div>
-  </template>
-
+  </transition>
 </template>
 
 <!-- SCRIPT -->
